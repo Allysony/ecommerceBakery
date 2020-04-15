c94def0a77a9ccc5834d072ec7364104eb448a1c
Richard Liu <richardbliu@gmail.com>
2020-04-15 16:44:31 -0700

    Official name implemented

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -1,16 +1,29 @@
 import React from "react";
 import bakers from "./bakers.png";
+import { Container, Row, Col } from "reactstrap";
 
 function HomeContent() {
   return (
-    <div>
-      <div>
-        <h3>Meet the Bakers</h3>
-        <p>Blah blah blah</p>
-      </div>
-      <img src={bakers} alt="The Bakers" />
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <div style={styles.bakerstext}>
+            <h3>Meet the Bakers</h3>
+            <p>Blah blah blah</p>
+          </div>
+        </Col>
+        <Col>
+          <img src={bakers} alt="The Bakers" width="400" />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
 export default HomeContent;
+
+let styles = {
+  bakerstext: {
+    textAlign: "right"
+  }
+};

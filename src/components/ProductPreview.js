27cd85fc2e557261356bc2ad83d3bfa3322f36f2
Allyson Yamasaki <allyson.yamasaki@gmail.com>
2020-04-17 16:12:07 -0700

    product hover and other things

--- src/components/ProductPreview.js
+++ src/components/ProductPreview.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, {Component} from "react";
 
-import { Card } from "react-bootstrap";
+import {Card} from "react-bootstrap";
 
+class ProductPreview extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {hovered: false};
+    }
 
-function ProductPreview(props) {
-  return (
-    <Card>
+    render() {
+        let linkStyle;
+        if (this.state.hovered) {
+            console.log("TRANSFORM!")
+            linkStyle = {transform: 'scale(1.5,1.5)'}
+        } else {
+            linkStyle = {}
+            console.log("BYE!")
+        }
+        return (
+            <Card style={{width: '30%', display: 'inline-block', margin: '1.5%'}}>
 
-      {/*<Card.Img variant="top" src={require(`${props.image}`)} /> <--need to fix image pass-through*/}
-      <Card.Img variant="top" src={props.image} />
-
-      <Card.Body>
-        <Card.Title>{props.name}</Card.Title>
-        <Card.Text>{props.price}</Card.Text>
-      </Card.Body>
-    </Card>
-  );
+                <a href={"/Cart"} >
+                    <Card.Img variant="top" src={this.props.image} style={linkStyle}
+                              onMouseEnter={() => this.setState({hovered: true})}
+                              onMouseLeave={() => this.setState({hovered: false})}/>
+                </a>
+                <Card.Body>
+                    <Card.Title>{this.props.name}</Card.Title>
+                    <Card.Text>{this.props.price}</Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    }
 }
+
 
 export default ProductPreview;

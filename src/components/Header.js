eef2870f5937ddd6877da30484499da738c5269c
Richard Liu <richardbliu@gmail.com>
2020-04-19 15:10:23 -0700

    Possible fix for navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,7 @@
 import React from "react";
-import {
-  Navbar,
-  Nav,
-  Form,
-  FormControl,
-  Button
-} from "react-bootstrap";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 import cupcake from "./cupcake.png";
 
 function Header() {
@@ -25,9 +21,15 @@ function Header() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/products">Products</Nav.Link>
-          <Nav.Link href="/cart">Cart</Nav.Link>
+          <Nav.Link as={Link} href="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} href="/products">
+            Products
+          </Nav.Link>
+          <Nav.Link as={Link} href="/cart">
+            Cart
+          </Nav.Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
